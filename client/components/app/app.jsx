e3d8ac939693e52e67bb8f185dec6387875ae34d
jianglirui <535904200@qq.com>
2018-07-22 13:21:35 +0800

    add app component

---
+++ client/components/app/app.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import styles from './app.css';
+
+class App extends React.Component {
+  render() {
+    const {
+      children,
+    } = this.props;
+    return (
+      <div className={styles.app}>
+        {children}
+      </div>
+    );
+  }
+}
+
+module.exports = connect()(App);
