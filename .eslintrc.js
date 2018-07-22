3403c9abcdc2718ff65cd864f6691c60088ecc4b
jianglirui <535904200@qq.com>
2018-07-22 13:22:06 +0800

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    "rules": {
+        "import/no-extraneous-dependencies": false,
+        "react/no-array-index-key": false,
+        "react/destructuring-assignment": false,
+        "react/prop-types": false,
+        "react/prefer-stateless-function": "warn",
+    },
 };
